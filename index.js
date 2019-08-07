ef4ad6d0345e1b9d89f4ac8662ed4e24e056da24
Nick Spera <nickaspera@gmail.com>
2019-08-07 01:58:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function sayHiToGrandma(string) {
 }
 
   if (string.toLowerCase() === string) {
-  return ("I can\'t hear you!")
+  return ("I can't hear you!")
 }
 
   if (string === "I love you, Grandma.") {
-  return ("I love you, too.");
+  return ("I love you, too.")
 }
 
   return "Are you eating enough?"
