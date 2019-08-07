cb1bcf136d33b0b007ca0f053f1313a9081e074f
Nick Spera <nickaspera@gmail.com>
2019-08-07 02:01:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log(string.toUpperCase))
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string){
-  console.log(string.toLowerCase))
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
-  return "YES INDEED!"
+    return "YES INDEED!"
 }
 
   if (string.toLowerCase() === string) {
-  return "I can't hear you!"
+    return "I can't hear you!"
 }
 
   if (string === "I love you, Grandma.") {
-  return "I love you, too."
+    return "I love you, too."
 }
 
   return "Are you eating enough?"
